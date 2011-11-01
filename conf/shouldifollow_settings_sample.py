d6ef140bef3511a54264f90c4075db9f13d2e525
Matt Ball <matt.ball.2@gmail.com>
2011-10-31 23:58:01 -0700

    non-working flask conf file -- port getting ignored

---
+++ conf/shouldifollow_settings_sample.py
@@ -0,0 +1,9 @@
+# the flask application's debug level.  /must/ be set to False for production
+DEBUG = False
+
+# generate a secret key with os.urandom(24)
+SECRET_KEY = 'immma secret'
+
+# local testing parameters
+APP_IP = '127.0.0.1'
+APP_PORT = 8000
